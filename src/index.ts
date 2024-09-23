046be12f73cd9eafbed426881d521a373753623e
Tiagofrk <bicalho.franck.tiago@gmail.com>
2024-09-23 09:07:20 -0300

    @hono/node-server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-// src/index.ts
 import { Hono } from 'hono';
 import { createHTTPHandler } from '@trpc/server/adapters/standalone';
+import { initTRPC } from '@trpc/server';
 import { adminRouter } from './routes/adminRouter';
 import { groupRouter } from './routes/groupRouter';
 import { postRouter } from './routes/postsRouter';
@@ -8,14 +8,25 @@ import { userRouter } from './routes/usersRouter';
 import { reactionRouter } from './routes/reactionRouter';
 import 'reflect-metadata';
 
+const t = initTRPC.create();
+
+const appRouter = t.mergeRouters(
+  adminRouter,
+  groupRouter,
+  postRouter,
+  userRouter,
+  reactionRouter,
+);
+
 const app = new Hono();
 
-app.use('/trpc/admin', createHTTPHandler({ router: adminRouter }));
-app.use('/trpc/group', createHTTPHandler({ router: groupRouter }));
-app.use('/trpc/post', createHTTPHandler({ router: postRouter }));
-app.use('/trpc/user', createHTTPHandler({ router: userRouter }));
-app.use('/trpc/reaction', createHTTPHandler({ router: reactionRouter }));
+app.use('/trpc', createHTTPHandler({
+  router: appRouter,
+  createContext: () => ({}),
+}));
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+export type AppRouter = typeof appRouter;
